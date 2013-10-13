9a054b80d76d304fd7619c587bb5b6169251973b
tda0909 <ct.jcpm@gmail.com>
2013-10-13 13:23:13 -0400

    Added Files for Future Use
    Config.yml
    plugin.yml
    New Event Listeners (Not Coded)

--- src/io/github/tda0909/ChargeSigns/CSCommands.java
+++ src/io/github/tda0909/ChargeSigns/CSCommands.java
@@ -8,24 +8,26 @@ import org.bukkit.entity.Player;
 public class CSCommands implements CommandExecutor {
 
 	private final ChargeSigns plugin;
-//	
+
 	public CSCommands(ChargeSigns plugin) {
 		this.plugin = plugin;
 	}
 
+	//Handles /chargesigns command
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		    if (cmd.getName().equalsIgnoreCase("chargesigns")) {
 		    	
 		    	if (!(sender instanceof Player)) {
-		    	//Player player = (Player) sender;
 				sender.sendMessage("Console, Command Run Confirmed");
 				
 		    	} else {
 		    		sender.sendMessage("Player, Command Run Confirmed");
-		    	return true;
+		    		return true;
 		    	}
 		    }
 			return false;
-		    
 	}
+	
+	
+	
 }
--- src/io/github/tda0909/ChargeSigns/ChargeSigns.java
+++ src/io/github/tda0909/ChargeSigns/ChargeSigns.java
@@ -1,6 +1,13 @@
 package io.github.tda0909.ChargeSigns;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.block.SignChangeEvent;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.entity.Player;
+import org.bukkit.configuration.file.YamlConfiguration;
 
 public final class ChargeSigns extends JavaPlugin{
 
@@ -17,4 +24,20 @@ public final class ChargeSigns extends JavaPlugin{
     public void onDisable() {
     	getLogger().info("[ChargeSigns] Plug-In Disabled");
     }
+    
+    
+    @EventHandler(priority=EventPriority.HIGH, ignoreCancelled=true)
+    public void OnSignChange(SignChangeEvent event){
+    	Player player = event.getPlayer();
+    	
+    	
+    	
+    }
+    
+    
+    @EventHandler(priority=EventPriority.HIGH, ignoreCancelled=true)
+    public void OnBlockPlace(BlockPlaceEvent event){
+    	
+    }
+    
 }
