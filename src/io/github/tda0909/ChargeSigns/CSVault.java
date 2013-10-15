f234e8797725eec079710e24ff95180740b13dc5
tda0909 <ct.jcpm@gmail.com>
2013-10-15 15:02:29 -0400

    Added Perms Update

--- src/io/github/tda0909/ChargeSigns/CSVault.java
+++ src/io/github/tda0909/ChargeSigns/CSVault.java
@@ -4,29 +4,19 @@ import net.milkbowl.vault.economy.Economy;
 import net.milkbowl.vault.permission.Permission;
 
 import org.bukkit.Bukkit;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
 import org.bukkit.plugin.RegisteredServiceProvider;
 
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
 
 public class CSVault {
 	
-	private static final Logger Log = null;
 	public ChargeSigns plugin;
-	public static Logger log = Logger.getLogger("Minecraft");
-	public static Economy econ = null;
-	public static Permission perm = null;
+	private static Logger log = Logger.getLogger("Minecraft");
+	private static Permission perm = null;
 	
 	
-	public CSVault(ChargeSigns instance) {
-		plugin = instance;
-	}
-	
-	//Function Initialized OnEnable to Check for & Hook into Vault Permissions
+	//Function Initialized by OnEnable to Check for & Hook into Vault Permissions
 	public static boolean VaultPerms() {
 		
 		RegisteredServiceProvider<Permission> permissionProvider = Bukkit.getServer().getServicesManager().getRegistration(Permission.class);
@@ -38,7 +28,7 @@ public class CSVault {
 	}
 	
 	
-	//Function Initialized OnEnable to Check for & Hook into Vault Economy
+	//Function Initialized by OnEnable to Check for & Hook into Vault Economy
 	public static boolean VaultEcon() {
 
 	    if (Bukkit.getServer().getPluginManager().getPlugin("Vault") == null) {
@@ -47,10 +37,11 @@ public class CSVault {
 	    RegisteredServiceProvider<Economy> rsp = Bukkit.getServer().getServicesManager().getRegistration(Economy.class);
 	    if (rsp == null) {
 	        return false;
+	     	        
 	    }
-	    econ = rsp.getProvider();
-	    log.info("[ChargeSigns] Hooked into Vault Economy " + econ.getName());
-	    return econ != null;
+	    ChargeSigns.economy = rsp.getProvider();
+	    log.info("[ChargeSigns] Hooked into Vault Economy " + ChargeSigns.economy.getName());
+	    return ChargeSigns.economy != null;
 	}
 
 	
