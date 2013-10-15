55c0637b59e0823dc44bab83d19e13d42f70f7cb
tda0909 <ct.jcpm@gmail.com>
2013-10-15 19:58:52 -0400

    Added Unimplemented Commands

--- src/io/github/tda0909/ChargeSigns/CSCommands.java
+++ src/io/github/tda0909/ChargeSigns/CSCommands.java
@@ -1,34 +1,103 @@
 package io.github.tda0909.ChargeSigns;
 
+
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.plugin.PluginDescriptionFile;
+
 
 public class CSCommands implements CommandExecutor {
 
-	private final ChargeSigns plugin;
+	public ChargeSigns plugin;
 	
-	//Sets Instance
+	// Sets Instance
 	public CSCommands(ChargeSigns plugin) {
 		this.plugin = plugin;
 	}
-	
-	
-	//Handles /chargesigns command
+
+
+	// Handles All /ChargeSigns Command
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		    if (cmd.getName().equalsIgnoreCase("chargesigns")) {
 		    	
-		    	if (!(sender instanceof Player)) {
-				sender.sendMessage("Console, Command Run Confirmed");
-				
-		    	} else {
-		    		sender.sendMessage("Player, Command Run Confirmed");
-		    		return true;
+		    	// Base Command to Show Plug-In is Active
+		    	if(args.length == 0){
+		    		if (!(sender instanceof Player)) {
+		    			sender.sendMessage("Console, Command Run Confirmed");
+		    			return true;
+		    			} else {
+		    				sender.sendMessage("Player, Command Run Confirmed");
+		    				return true;
+		    				}		    			
+		    		}
+		    	
+		    	// ChargeSigns Version - Shows Current PlugIn Version
+		    	if(args.length == 1){
+		    		
+		    		if (args[0].equalsIgnoreCase("version")){
+		    			//String ver = ChargeSigns.version.getVersion();
+		    			//String var = PluginDescriptionFile.getVersion();
+		    			sender.sendMessage("Not Yet Implemented");
+		    			return true;
+		    		}
+		    		
+		    		else if (args[0].equalsIgnoreCase("reload")){
+		    			sender.sendMessage("Reload Not Yet Implemented");
+		    			return true;
+		    		} 
+		    		
+		    		else if (args[0].equalsIgnoreCase("cost")){
+		    			sender.sendMessage("Cost Not Yet Implemented");
+		    			return true;
+		    		}
+		    		
+		    		else if (args[0].equalsIgnoreCase("add")){
+		    			sender.sendMessage("Add Not Yet Implemented");
+		    			return true;
+		    		}
+		    		
+		    		else if (args[0].equalsIgnoreCase("modify")){
+		    			sender.sendMessage("Modify Not Yet Implemented");
+		    			return true;
+		    		}
+		    		
+		    		else if (args[0].equalsIgnoreCase("remove")){
+		    			sender.sendMessage("Remove Not Yet Implemented");
+		    			return true;
+		    		}
+		    		
+		    		else if (args[0].equalsIgnoreCase("exempt")){
+		    			sender.sendMessage("Exempt Not Yet Implemented");
+		    			return true;
+		    		}
+		    		
+		    		else if (args[0].equalsIgnoreCase("norefund")){
+		    			sender.sendMessage("NoRefund Not Yet Implemented");
+		    			return true;
+		    		}
+		    		
+		    		else {
+		    				sender.sendMessage("Invalid Command Arguments");
+		    				return true;
+		    		}
+		    		
 		    	}
+		    	
+		    	// If Wrong Number of Arguments
+		    	if(args.length > 1)
+		    		sender.sendMessage("Invalid Command Arguments");
+		    	
+		    	
+		    	
 		    }
+		    
+		    
 			return false;
 	}
+	
+	
 	
 	
 	
